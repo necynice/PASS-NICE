946d8faa3c6c977f84c9336c8c54b5fc2627f135
necynice <seungeun0907@naver.com>
2025-05-04 22:11:43 +0900

    v1.1.0 대응

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from src.PASS_NICE import PASS_NICE
 
 async def main():
     # ISP
-    name, birthdate, phone, ISP = "홍길동", "0102034", "01012345678", "KT"
+    name, birthdate, phone, ISP = "홍길동", "0000000", "01012345678", "SM"
     
     try:
         verification = PASS_NICE(ISP)
@@ -11,11 +11,11 @@ async def main():
     except:
         return {"Success": False, "Message": "올바른 ISP 값을 입력해주세요."}
     
-    initResult = await verification.init_session()
+    initResult = await verification.initSession()
     if not initResult['Success']:
         return {"Success": False, "Message": initResult['Message']}
 
-    captchaResult = await verification.get_captcha()
+    captchaResult = await verification.getCaptcha()
     if not captchaResult['Success']: return {"Success": False, "Message": captchaResult['Message']}
     try:
         with open('captcha.png', 'wb') as f:
@@ -25,12 +25,12 @@ async def main():
         return {"Success": False, "Message": "캡챠 이미지를 저장하던 중 오류가 발생하였습니다."}
 
     captchaCode = input("캡챠 코드를 입력해주세요: ")
-    sendResult = await verification.send_SMS_verify(name, birthdate, phone, captchaCode)
+    sendResult = await verification.sendSMS(name, birthdate, phone, captchaCode)
     if not sendResult['Success']:
         return {"Success": False, "Message": sendResult['Message']}
     
     smsCode = input("SMS로 전송된 코드를 입력해주세요: ")
-    checkResult = await verification.check_SMS_verify(smsCode)
+    checkResult = await verification.checkSMS(smsCode)
     if not checkResult['Success']:
         return {"Success": False, "Message": checkResult['Message']}
 
